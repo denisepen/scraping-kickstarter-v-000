74df3986792edf0f1e760ca5a52d541a7b19eb3b
Denise Pendleton <dpendl1@hotmail.com>
2018-03-10 22:59:09 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -24,7 +24,7 @@ require 'pry'
         :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
       }
     end
-    puts project
+    puts projects
   end
 
 
